4ee7ad461a6eb10651d8a711229e4c96793831c4
Dai Jian <jianacad@outlook.com>
2024-10-26 00:08:16 +0800

    Match body region.

--- cavass/slice_range.py
+++ cavass/slice_range.py
@@ -5,18 +5,18 @@ from jbag.log import logger
 from cavass.ops import get_slice_number
 
 
-def get_slice_range(portion_file: Union[str, LiteralString], entire_region_file: Union[str, LiteralString]):
+def get_slice_range(partial_region_file: Union[str, LiteralString], entire_region_file: Union[str, LiteralString]):
     """
     Get the slice location range where the `portion file` is located in the `entire region file`.
 
     Args:
-        portion_file (str or LiteralString):
+        partial_region_file (str or LiteralString):
         entire_region_file (str or LiteralString):
 
     Returns:
 
     """
-    slice_number_1 = get_slice_number(portion_file)
+    slice_number_1 = get_slice_number(partial_region_file)
     slice_number_2 = get_slice_number(entire_region_file)
     # inferior slice refers to the index of slice in input_file_2 where
     # the inferior slice (the first slice) of input_file_1 located in input_file_2.
@@ -30,8 +30,8 @@ def get_slice_range(portion_file: Union[str, LiteralString], entire_region_file:
             slice_number_1[4] != slice_number_2[4]) or (slice_number_1[6] != slice_number_2[6]) or (
             slice_number_1[7] != slice_number_2[7]):
         logger.warning(
-            f'Input files do not match.\nInput file 1 is {portion_file}.\nInput file 2 is {entire_region_file}.')
-        unmatched = [portion_file, entire_region_file]
+            f'Input files do not match.\nInput file 1 is {partial_region_file}.\nInput file 2 is {entire_region_file}.')
+        unmatched = [partial_region_file, entire_region_file]
     else:
         unmatched = []
     return inferior_slice_idx, superior_slice_idx, unmatched
